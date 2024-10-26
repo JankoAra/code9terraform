028ecb57c4c1cce2e11a2563191df8ea7f83183d
JankoAra <janko.arandjelovic@gmail.com>
2024-10-26 12:42:12 +0200

    promena imena resursa

--- terraform/s3-site/s3-objects.tf
+++ terraform/s3-site/s3-objects.tf
@@ -17,7 +17,7 @@ resource "aws_s3_object" "css" {
   source = "templates/styles.css"
 }
 # Upload styles.css to s3 bucket
-resource "aws_s3_object" "css" {
+resource "aws_s3_object" "css1" {
   bucket = aws_s3_bucket.my-static-website.id
   key    = "janko.css"
   source = "templates/janko.css"
